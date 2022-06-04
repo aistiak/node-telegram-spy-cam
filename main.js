c86ab37525a6e82cc069c61a9c2898ec2369e63d
Arif Istiak <aristiak1994@gmail.com>
2022-06-04 13:23:55 +0600

    without chat-id

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var takePic = require("./takePic");
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
-const { TELEGRAM_TOKEN, MY_TELEGRAM_CHAT_ID, ACTIVATION_TEXT } = process.env
+const { TELEGRAM_TOKEN, ACTIVATION_TEXT } = process.env
 
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
@@ -9,7 +9,7 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const { text } = msg;
     try {
-        if (chatId == MY_TELEGRAM_CHAT_ID && text == ACTIVATION_TEXT) {
+        if (text == ACTIVATION_TEXT) {
             bot.sendMessage(chatId, 'wait');
             const filename = await takePic();
             const url = `${__dirname}/${filename}`;
